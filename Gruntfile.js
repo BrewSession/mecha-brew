72e03b68f1a1a87d43c902f8d73278c9db0256f2
GregRR <greg.r.roe@gmail.com>
2015-03-03 16:20:06 -0800

    Use PureCSS instead of Foundation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,32 +2,38 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      js: {
-        'build/js/mecha-brew.min.js': ['src/js/app.js'],
-        'build/js/lib/modernizr.min.js': ['bower_components/foundation/js/vendor/foundation.js']
-      },
-    },
-	copy: {
-      files: {
-        src: 'src/index.html',
-        dest: 'build/index.html'
-      },
-    },
     bowercopy: {
         options: {
         	runBower: true
         },
         libs: {
             options: {
-                destPrefix: 'build/js/lib'
+                destPrefix: 'build'
             },
             files: {
-                'jquery.min.js': 'jquery/dist/jquery.min.js',
-                'foundation.min.js': 'foundation/js/foundation.min.js',
-                'rem.min.js': 'rem-unit-polyfill/js/rem.min.js'
+                'js/lib/rem.min.js': 'rem-unit-polyfill/js/rem.min.js'
             },
+            folders: {
+                'fonts': 'font-awesome/fonts'
+            }
         }
+    },
+    rename: {
+        css: {
+            src: 'bower_components/pure/pure.css',
+            dest: 'bower_components/pure/_pure.scss'
+        }
+    },
+    uglify: {
+      js: {
+        'build/js/mecha-brew.min.js': ['src/js/app.js']
+      },
+    },
+	copy: {
+      html: {
+        src: 'src/index.html',
+        dest: 'build/index.html'
+      },
     },
     sass: {                             
 		dist: {                           
@@ -36,17 +42,17 @@ module.exports = function(grunt) {
 		  },
 		  files: {                         
 			'build/css/style.css': 'src/scss/style.scss',
-			'build/css/normalize.css': 'bower_components/foundation/scss/normalize.scss'
 		  }
 		}
 	}
   });
 
+  grunt.loadNpmTasks('grunt-bowercopy');
+  grunt.loadNpmTasks('grunt-rename');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-copy');
-  grunt.loadNpmTasks('grunt-bowercopy');
   grunt.loadNpmTasks('grunt-contrib-sass');
   
-  grunt.registerTask('default', ['uglify', 'copy', 'sass', 'bowercopy']);
+  grunt.registerTask('default', ['bowercopy', 'rename', 'uglify', 'copy', 'sass']);
 
 };
